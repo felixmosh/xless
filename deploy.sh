12142d8c39249cf650f603464868bf2109bd5ba1
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-01-05 15:06:43 +0200

    Change env

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-read -p "Enter Slack Incoming Hook: " "slack_incoming_webhook"
-vercel secret add slack-incoming-webhook "$slack_incoming_webhook"
+read -p "Enter Discord Incoming Hook: " "discord_incoming_webhook"
+vercel secret add discord-incoming-webhook "$discord_incoming_webhook"
 
 read -p "Enter IMGBB API Key: " "imgbb_api_key"
 vercel secret add imgbb-api-key "$imgbb_api_key"
